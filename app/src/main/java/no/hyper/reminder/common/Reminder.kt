ec63d5dc70a64dca8027073e36309d068bba3e2b
Jean <jean.tuffier@gmail.com>
2016-11-25 15:52:14 +0100

    sign app for release

--- app/src/main/java/no/hyper/reminder/common/Reminder.kt
+++ app/src/main/java/no/hyper/reminder/common/Reminder.kt
@@ -2,6 +2,7 @@ package no.hyper.reminder.common
 
 import android.app.Application
 import android.content.Context
+import com.squareup.leakcanary.LeakCanary
 import no.hyper.reminder.common.injection.ApplicationComponent
 import no.hyper.reminder.common.injection.ApplicationModule
 import no.hyper.reminder.common.injection.DaggerApplicationComponent
@@ -20,6 +21,7 @@ class Reminder : Application() {
     override fun onCreate() {
         super.onCreate()
         initApplicationComponent()
+        LeakCanary.install(this)
     }
 
     private fun initApplicationComponent() {
