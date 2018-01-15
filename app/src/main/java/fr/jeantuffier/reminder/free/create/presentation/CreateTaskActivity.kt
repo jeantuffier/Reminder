5fd091315978bca2ddf364d7d3f4fe6616d6a631
Jean <jean.tuffier@gmail.com>
2018-01-15 09:26:54 +0100

    init ui

--- app/src/main/java/fr/jeantuffier/reminder/free/create/presentation/CreateTaskActivity.kt
+++ app/src/main/java/fr/jeantuffier/reminder/free/create/presentation/CreateTaskActivity.kt
@@ -1,14 +1,17 @@
 package fr.jeantuffier.reminder.free.create.presentation
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentStatePagerAdapter
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
+import android.widget.ArrayAdapter
+import android.widget.SeekBar
 import dagger.android.AndroidInjection
 import fr.jeantuffier.reminder.R
 import fr.jeantuffier.reminder.free.common.extension.toDp
-import fr.jeantuffier.reminder.free.create.view.CreateTaskMandatoryFragment
+import fr.jeantuffier.reminder.free.common.utils.SimpleSeekBarChangeListener
 import fr.jeantuffier.reminder.free.create.view.CreateTaskTimeFragment
 import fr.jeantuffier.reminder.free.home.presentation.HomeActivity
 import kotlinx.android.synthetic.main.create_task_activity.*
@@ -16,9 +19,9 @@ import javax.inject.Inject
 
 class CreateTaskActivity : AppCompatActivity(), CreateTaskContract.View {
 
-    private val mandatoryFragment = CreateTaskMandatoryFragment()
+    //private val mandatoryFragment = CreateTaskMandatoryFragment()
     private val timeFragment = CreateTaskTimeFragment()
-    private val fragments = arrayOf(mandatoryFragment, timeFragment)
+    //private val fragments = arrayOf(mandatoryFragment, timeFragment)
 
     @Inject
     lateinit var presenter: CreateTaskContract.Presenter
@@ -26,8 +29,11 @@ class CreateTaskActivity : AppCompatActivity(), CreateTaskContract.View {
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.create_task_activity)
         setToolbar()
+        setFrequencySpinner()
+        setPriorityBar()
 
         form_create_button.setOnClickListener { createTask() }
     }
@@ -44,13 +50,35 @@ class CreateTaskActivity : AppCompatActivity(), CreateTaskContract.View {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
+    private fun setFrequencySpinner() {
+        val frequencies = arrayListOf(getString(R.string.create_task_frequency_hours),
+                getString(R.string.create_task_frequency_minutes))
+        val adapter =  ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, frequencies)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        task_frequency.adapter = adapter
+        task_frequency.setSelection(0)
+    }
+
+    private fun setPriorityBar() {
+        val priority = getString(R.string.create_task_priority,
+                getString(R.string.create_task_priority_middle).toLowerCase())
+        task_priority_progress.text = priority
+        task_priority.setOnSeekBarChangeListener(SimpleSeekBarChangeListener { progress ->
+            task_priority_progress.text = when (progress) {
+                0 -> getString(R.string.create_task_priority, getString(R.string.create_task_priority_low).toLowerCase())
+                1 -> getString(R.string.create_task_priority, getString(R.string.create_task_priority_middle).toLowerCase())
+                else -> getString(R.string.create_task_priority, getString(R.string.create_task_priority_high).toLowerCase())
+            }
+        })
+    }
+
     private fun createTask() {
-        val title = mandatoryFragment.getTitle()
+        /*val title = mandatoryFragment.getTitle()
         val delay = mandatoryFragment.getDelay().toInt()
         val frequency = mandatoryFragment.getFrequency()
         val priority = mandatoryFragment.getPriority()
         val time = timeFragment.getTaskTime()
-        presenter.createTask(this, title, delay, frequency, priority, time)
+        presenter.createTask(this, title, delay, frequency, priority, time)*/
     }
 
 }
